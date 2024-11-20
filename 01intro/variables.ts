aaf934c3afd8f7d7b7584f8f053157debd7be992
Akhilesh Chandewar <akhil.chandewar00@gmail.com>
2024-11-20 14:02:52 +0530

    introduction with deno 2

---
+++ 01intro/variables.ts
@@ -0,0 +1,18 @@
+let greeting : string = "Hello, World!";
+console.log(greeting);
+console.log(greeting.toUpperCase()); 
+
+let userId : number = 223666.7;
+console.log(userId.toFixed(2));
+
+let isLoggedIn : boolean = false;
+console.log(isLoggedIn.valueOf());
+
+let hero : any;
+function getHero() : string {
+    return "Superman";
+}
+
+hero = getHero();
+console.log(hero);
+
